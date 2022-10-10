c295e1c42ab3e89f48a16eec4b50a36c50471f24
Benjamin Edwards <ben.edwards@jada-development.com>
2022-10-10 23:00:00 +0100

    Added basic web structure

---
+++ almon-web/almon-web-core/src/main/java/com/capgemini/bedwards/almon/almonwebcore/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.capgemini.bedwards.almon.almonwebcore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    @GetMapping({"home", "/"})
+    public String home(Map<String, Object> model) {
+        model.put("message","Hello Ben");
+        return "home";
+    }
+}
