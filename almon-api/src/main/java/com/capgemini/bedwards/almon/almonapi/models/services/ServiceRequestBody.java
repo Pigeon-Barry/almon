c3b5b6af58a0d6342dec65fa5127f456862ce2b9
Benjamin Edwards <ben.edwards@jada-development.com>
2022-12-03 21:47:11 +0000

    Minor code fix

--- almon-api/src/main/java/com/capgemini/bedwards/almon/almonapi/models/services/ServiceRequestBody.java
+++ almon-api/src/main/java/com/capgemini/bedwards/almon/almonapi/models/services/ServiceRequestBody.java
@@ -2,18 +2,17 @@ package com.capgemini.bedwards.almon.almonapi.models.services;
 
 import com.capgemini.bedwards.almon.almoncore.validators.ServiceDoesNotExist;
 import com.capgemini.bedwards.almon.almondatastore.util.Constants;
-import lombok.Data;
-import lombok.ToString;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import lombok.Data;
+import lombok.ToString;
 
 @Data
 @ToString
 public class ServiceRequestBody {
 
     @NotBlank
-    @Pattern(regexp = Constants.SERVICE_REGEX, message = Constants.SERVICE_REGEX_INVALID_MESSAGE)
+    @Pattern(regexp = Constants.SERVICE_KEY_REGEX, message = Constants.SERVICE_KEY_REGEX_INVALID_MESSAGE)
     @ServiceDoesNotExist(message = "A Service with this key already exists")
     private String key;
 
--- almon-web/src/main/java/com/capgemini/bedwards/almon/almonweb/model/services/ServiceRequestBody.java
+++ almon-web/src/main/java/com/capgemini/bedwards/almon/almonweb/model/services/ServiceRequestBody.java
@@ -2,17 +2,16 @@ package com.capgemini.bedwards.almon.almonweb.model.services;
 
 import com.capgemini.bedwards.almon.almoncore.validators.ServiceDoesNotExist;
 import com.capgemini.bedwards.almon.almondatastore.util.Constants;
-import lombok.Data;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import lombok.Data;
 
 @Data
 public class ServiceRequestBody {
 
     @NotBlank
-    @Pattern(regexp = Constants.SERVICE_REGEX, message = Constants.SERVICE_REGEX_INVALID_MESSAGE)
+    @Pattern(regexp = Constants.SERVICE_KEY_REGEX, message = Constants.SERVICE_KEY_REGEX_INVALID_MESSAGE)
     @Size(max = Constants.SERVICE_ID_MAX_LENGTH)
     @ServiceDoesNotExist(message = "A Service with this key already exists")
     private String key;
